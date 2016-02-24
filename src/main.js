059a0014fea91658ea75ee203fd2b003d63549c9
lwxyfer <lwxyfer@gmail.com>
2016-02-24 17:19:22 +0800

    performance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var $ = window.jQuery = require('jquery')
-require('./css/icon.css')
-require('./materialize/css/materialize.min.css')
+// require('./css/icon.css')
+// require('./materialize/css/materialize.min.css')
 require('./materialize/js/materialize.min.js')
 require('./css/style.css')
 var Vue = require("vue")
@@ -8,17 +8,25 @@ var myData = require("./data.js")
 Vue.config.debug = true
 
 var NavData = new Vue({
-	el:"main",
-	data:myData
+	el: "main",
+	data: myData
 })
 var siteSearch = new Vue({
-	el:"nav",
-	data:{
-		site:"Inner Search"
-	}
+		el: "nav",
+		data: {
+			site: "Inner Search"
+		}
+	})
+	// $('#searchTog').click(function() {
+	// 	$('#searchInner').slideToggle(1000)
+	// })
+$(document).ready(function() {
+	$(".button-collapse").sideNav();
+	$('select').material_select();
+	$(document).keydown(function(e) {
+		var e = e || window.event;
+		if (e.ctrlKey && e.keyCode == 13) {
+			$('#search').focus()
+		}
+	})
 })
-// $('#searchTog').click(function() {
-// 	$('#searchInner').slideToggle(1000)
-// })
-$(".button-collapse").sideNav();
-$('select').material_select();
